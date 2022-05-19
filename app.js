600f18ddc2783944e02bf0769700bca7b7886889
Shubham Meshram <meshram.shubham27@gmail.com>
2022-05-19 19:41:39 +0530

    random pattern generation

--- app.js
+++ app.js
@@ -5,21 +5,40 @@ context.fillStyle = "blue";
 // context.fillRect(100, 100, 400, 400);
 
 context.lineWidth = 4;
-context.beginPath();
-context.rect(100, 100, 400, 400);
-context.stroke();
+// context.beginPath();
+// context.rect(100, 100, 400, 400);
+// context.stroke();
 
-context.beginPath();
-context.arc(300, 300, 100, 0, Math.PI * 2);
-context.stroke();
+// context.beginPath();
+// context.arc(300, 300, 100, 0, Math.PI * 2);
+// context.stroke();
 
-for (let i = 0; i < 5; i++) {
-  let width = 60;
-  let height = 60;
-  let x = 100;
-  let y = 100;
+const width = 80;
+const height = 80;
+const gap = 20;
 
-  context.beginPath();
-  context.rect(100 + 80 * i, 100, width, height);
-  context.stroke();
+function generateRectangle() {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      let x = 100 + (width + gap) * i;
+      let y = 100 + (height + gap) * j;
+
+      context.beginPath();
+      context.rect(x, y, width, height);
+      context.stroke();
+
+      const randomNumber = Math.random();
+      if (randomNumber > 0.5) {
+        context.beginPath();
+        context.rect(x + 8, y + 8, width - 16, height - 16);
+        context.stroke();
+      }
+    }
+  }
+
+//   setInterval(generateRectangle, 1500);
 }
+
+generateRectangle();
+
+// setInterval(generateRectangle, 1500);
