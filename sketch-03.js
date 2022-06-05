9dfc006213e69af97383448ef5f978968d61fd5d
Shubham Meshram <meshram.shubham27@gmail.com>
2022-06-05 21:29:46 +0530

    created animation from points

--- sketch-03.js
+++ sketch-03.js
@@ -3,6 +3,7 @@ const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true,
 };
 
 const sketch = ({ context, width, height }) => {
@@ -17,6 +18,7 @@ const sketch = ({ context, width, height }) => {
     context.fillRect(0, 0, width, height);
 
     agents.forEach(agent => {
+      agent.update();
       agent.draw(context);
     })
 
@@ -30,7 +32,7 @@ const sketch = ({ context, width, height }) => {
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -39,15 +41,28 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(-1,1),random.range(-1,1))
     this.radius = random.range(4,12);
   }
 
-  draw(context) {
-    context.fillStyle = "black";
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
 
+  draw(context) {
+    // context.fillStyle = "black";
+
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 4;
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
+
+    context.restore();
   }
 }
